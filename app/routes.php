c70f4d506e2681d7d98b5ee1b794ca9469ba4398
khiser <hiserk@gmail.com>
2014-10-27 17:07:48 -0400

    version1

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,6 @@
 <?php
-
+	
+use Endroid\QrCode\QrCode;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,7 +12,42 @@
 |
 */
 
-Route::get('/', function()
+Route::match(array('GET', 'POST'), '/', function(){
+
+if(Input::has('submit')){
+	$AccountSid = "AC248077508b8520426a8d0a27619d3a93";
+	$AuthToken = "8d13609077ebce3135c691e6987e87f0";
+	$client = new Services_Twilio($AccountSid, $AuthToken);
+
+	$sms = $client->account->messages->sendMessage(
+		"+16149024463",
+		Input::get('phone_num'),
+		"Congrats! Here is your ticket.",
+		"http://seedmissions.com/qrcode?name=".urlencode(Input::get('name')) . "&phone_num=" . Input::get('phone_num')
+		);
+}
+
+	return View::make('home');
+});
+
+Route::get('/qrcode', Function()
 {
-	return View::make('hello');
+	$name = Input::get('name');
+	$number = Input::get('phone_num');
+	$code = base64_encode($name . $number);
+	$QrCode = new QrCode();
+	$QrCode->setText($code);
+	$image = $QrCode->get();
+	$response = Response::make(
+		$image,
+		 200
+ 	);
+
+ 	$response->header(
+ 		'content-type',
+ 		'image/png'
+ 	);
+
+ return $response;
+
 });
---
+++ app/views/home.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Ticket Distribution System</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div>
+      <h1>Ticket Distribution</h1>
+      <form method="POST">
+        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="phone_num" placeholder="Phone Number" />
+        <input type="submit" name="submit" value="Send Ticket" />
+      </form>
+    </div>  
+  </body>
+</html>  
